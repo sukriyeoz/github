309d96461b8eeada2b460b33fe3c7cfc165d25b2
sukriyeoz <sukriyekusoz@gmail.com>
2021-08-01 15:40:42 -0400

    second commit

--- src/package1/ClassOne.java
+++ src/package1/ClassOne.java
@@ -3,5 +3,7 @@ package package1;
 public class ClassOne {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("Batch 35/36 learn Github");
+        System.out.println("yeni");
     }
 }
