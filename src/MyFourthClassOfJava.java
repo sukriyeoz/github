99701409514911cd92b5cf320407f8db898a85cd
sukriyeoz <sukriyekusoz@gmail.com>
2021-08-07 15:43:13 -0400

    src/MythirdClass.java

--- src/MyFourthClassOfJava.java
+++ src/MyFourthClassOfJava.java
@@ -1,2 +1,10 @@
 public class MyFourthClassOfJava {
-}
+    public static void main(String [] args){
+        int x=543;
+        int sum=x+;
+
+        System.out.println(sum);
+
+
+
+    }
--- src/MythirdClass.java
+++ src/MythirdClass.java
@@ -1,11 +1,12 @@
 public class MythirdClass {
-
     public static void main(String [] args){
         int x=5;
         int sum=x+2;
-
         System.out.println(sum);
-        
+        int y=0;
+
+        String str="Java";
+
         
        System.out.println("this is remote repo code");
     }
